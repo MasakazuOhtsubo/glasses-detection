5cea09cd2d6fda77dfec82698752a9087284a675
ohtsubo <ohtsubo@e3sys.co.jp>
2024-09-17 18:42:08 +0900

    debug用の出力を追加

--- app/main.js
+++ app/main.js
@@ -12,6 +12,7 @@ let currentStream;
 // デバイスリストの取得とカメラ選択ドロップダウンに表示
 navigator.mediaDevices.enumerateDevices().then(devices => {
     const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    console.log('カメラデバイス一覧:', videoDevices);
     videoDevices.forEach(device => {
         const option = document.createElement('option');
         option.value = device.deviceId;
@@ -21,12 +22,14 @@ navigator.mediaDevices.enumerateDevices().then(devices => {
 
     if (videoDevices.length > 0) {
         // 初期カメラの設定
+        console.log('初期カメラ:', videoDevices[0].deviceId);
         startCamera(videoDevices[0].deviceId);
     }
 });
 
 // カメラ変更イベント
 cameraSelect.onchange = () => {
+    console.log('選択されたカメラ:', cameraSelect.value);
     startCamera(cameraSelect.value);
 };
 
@@ -34,6 +37,7 @@ cameraSelect.onchange = () => {
 function startCamera(deviceId) {
     if (currentStream) {
         // 前のストリームを停止
+        console.log('前のストリームを停止します');
         currentStream.getTracks().forEach(track => track.stop());
     }
 
@@ -42,19 +46,37 @@ function startCamera(deviceId) {
     }).then(stream => {
         currentStream = stream;
         video.srcObject = stream;
+
+        console.log('カメラストリームが開始されました:', stream);
+
+        // 動画の再生が準備できたら物体検出を開始
+        video.onloadedmetadata = () => {
+            video.play();  // カメラ映像の再生を開始
+            console.log('ビデオのメタデータがロードされました');
+
+            if (!model) {
+                // モデルのロードが完了していない場合、モデルをロード
+                console.log('モデルをロードしています...');
+                cocoSsd.load().then(loadedModel => {
+                    model = loadedModel;
+                    console.log('モデルがロードされました');
+                    detectObjects();  // 検出処理を開始
+                });
+            } else {
+                console.log('既存のモデルを使用して検出を開始します');
+                detectObjects();  // モデルが既にロード済みの場合は検出を開始
+            }
+        };
     }).catch(err => {
         console.error("カメラの起動に失敗しました: ", err);
     });
 }
 
-// coco-ssdモデルをロードして物体検出を開始
-cocoSsd.load().then(loadedModel => {
-    model = loadedModel;
-    detectObjects();
-});
-
+// 検出処理の実行
 function detectObjects() {
+    console.log('物体検出を開始します');
     model.detect(video).then(predictions => {
+        console.log('物体検出の結果:', predictions);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         predictions.forEach(prediction => {
@@ -73,7 +95,9 @@ function detectObjects() {
             }
         });
 
-        // 再帰的に検出を繰り返す
+        // 次のフレームで再び検出
         requestAnimationFrame(detectObjects);
+    }).catch(err => {
+        console.error("物体検出中にエラーが発生しました: ", err);
     });
 }
